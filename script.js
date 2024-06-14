fb33f5ba6ba7def52d56a371d519e353498a1282
4kujo <hot.ershik@gmail.com>
2024-06-14 11:28:30 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,13 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    var buttons = document.querySelectorAll('.myBtn');
-    buttons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            var id = this.getAttribute('data-id');
-            fetchDataFromPHP(id);
+    document.querySelectorAll('.myBtn').forEach(button => {
+        button.addEventListener('click', () => {
+            const id = button.getAttribute('data-id');
+            document.querySelectorAll('.block').forEach(block => {
+                block.style.display = 'none';
+            });
+            document.getElementById(`block${id}`).style.display = 'block';
         });
     });
-});
-
-function fetchDataFromPHP(id) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var response = JSON.parse(xhr.responseText);
-                displayData(response);
-            } else {
-                console.error('Произошла ошибка при выполнении запроса:', xhr.status);
-            }
-        }
-    };
-    xhr.open('GET', 'fetch_data.php?id=' + id, true);
-    xhr.send();
-}
-
-function displayData(data) {
-    var outputDiv = document.getElementById('output');
-    outputDiv.innerHTML = '<div class="base-color"><img src="img/' + data.img + ' " class="out"><h5>' + data.h1 + '</h5><ul><li><p>' + data.p1 + '</p></li><li><p>' + data.p2 + '</p></li><ul></div>';
-}
 
 
 
@@ -88,15 +67,15 @@ $(document).ready(function() {
 document.addEventListener('DOMContentLoaded', function() {
     var showHideBtn = document.getElementById('showHideBtn');
 
-    // Проверка наличия кнопки с идентификатором 'showHideBtn'
+    
     if (showHideBtn) {
-        // Если кнопка существует, добавляем обработчик события
+      
         showHideBtn.addEventListener('click', function() {
             if (showHideBtn.textContent === 'Показать еще') {
-                // Если текст кнопки "Показать еще", изменяем его на "Скрыть"
+                
                 showHideBtn.textContent = 'Скрыть';
             } else {
-                // Если текст кнопки "Скрыть", изменяем его на "Показать еще"
+                
                 showHideBtn.textContent = 'Показать еще';
             }
         });
